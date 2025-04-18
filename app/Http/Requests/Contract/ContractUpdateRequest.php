24c2af73caa96ec0ffbfa1fe739af2c2ec1d6d49
silverwind31 <116893365+silverwind31@users.noreply.github.com>
2025-04-18 08:28:11 +0500

    finished

--- app/Http/Requests/Contract/ContractUpdateRequest.php
+++ app/Http/Requests/Contract/ContractUpdateRequest.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Requests\Contract;
 
+use App\Models\Currency;
+use App\Models\User;
+use App\Models\Department;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ContractUpdateRequest extends FormRequest
@@ -39,7 +42,38 @@ class ContractUpdateRequest extends FormRequest
             'deleted_old_file_ids' => ['nullable', 'array'],
             'deleted_old_file_ids.*' => ['integer'],
 
-            'recipients' => ['required', 'array'],
+            'recipients' => [
+                'required',
+                'array',
+                function ($attribute, $value, $fail) {
+                    if (!is_array($value) || count($value) < 2) {
+                        $fail('Необходимо выбрать минимум 2 получателя.');
+                    }
+
+                    $currencyId = $this->input('currency_id');
+                    $currency = Currency::find($currencyId);
+
+                    $departments = [7, 8];
+                    $usersByDepartment = User::whereIn('department_id', $departments)->whereIn('id', $value)->pluck('department_id')->toArray();
+
+                    foreach ($departments as $departmentId) {
+                        $departmentName = Department::find($departmentId)->name ?? trans('app.label.unknown_department');
+                        if (!in_array($departmentId, $usersByDepartment)) {
+                            $fail(trans('app.label.select_recipient_from_department', ['department' => $departmentName]));
+                        }
+                    }
+
+                    if ($currency && strtoupper($currency->short_name) !== 'UZS') {
+                        $hasAccountant = User::whereIn('id', $value)
+                            ->where('department_id', 9)
+                            ->exists();
+
+                        if (!$hasAccountant) {
+                            $fail(trans('app.label.select_accounting_recipient'));
+                        }
+                    }
+                }
+            ],
             'recipients.*' => ['integer', 'exists:users,id'],
         ];
     }
@@ -49,8 +83,10 @@ class ContractUpdateRequest extends FormRequest
         return [
             'recipients.required' => trans('app.label.recipients_required'),
             'files.*.mimes' => trans('app.label.files_invalid_type'),
+            'files.min' => trans('app.label.files_min'),
             'files.*.max' => trans('app.label.files_max_size'),
             'files.required' => trans('app.label.files_required'),
+            'budget_sum.min' => trans('app.label.budget_nonzero'),
         ];
     }
 }
